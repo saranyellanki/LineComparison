904f77db42cfb9f79c4fa90138fdc9c6ea0e17e1
saranyellanki <saran.yellanki12@gmail.com>
2021-10-25 14:23:00 +0530

    Added Line Comparison

---
+++ src/main/java/com/bridgelabz/UC4/LineComparison.java
@@ -0,0 +1,63 @@
+package com.bridgelabz.UC4;
+
+import java.util.Scanner;
+
+import static java.lang.Double.compare;
+
+public class LineComparison {
+    int x1, x2, y1, y2;
+    Double lengthOfLine;
+    static int finalResult;
+
+    public LineComparison(int x1, int x2, int y1, int y2) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+    public LineComparison calculateLengthOfLine() {
+        this.lengthOfLine = Math.sqrt(Math.pow((this.x1 - this.x2), 2) + Math.pow((this.y1 - this.y2), 2));
+        return this;
+    }
+    public void show() {
+        System.out.printf("The length of line formed by co-ordinates ( %d, %d),( %d, %d) is %f %n", this.x1, this.x2, this.y1, this.y2, this.lengthOfLine);
+    }
+    public static int compareLines(LineComparison result1, LineComparison result2) {
+        return compare(result1.lengthOfLine, result2.lengthOfLine);
+    }
+    public static void lineCoOrdinates(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the co-ordinate x1");
+        int x1 = sc.nextInt();
+        System.out.println("Enter the co-ordinate y1");
+        int y1 = sc.nextInt();
+        System.out.println("Enter the co-ordinate x2");
+        int x2 = sc.nextInt();
+        System.out.println("Enter the co-ordinate y2");
+        int y2 = sc.nextInt();
+        LineComparison result1 = new LineComparison(x1, x2, y1, y2);
+        result1.calculateLengthOfLine().show();
+
+        System.out.println("Enter the co-ordinate x3");
+        int x3 = sc.nextInt();
+        System.out.println("Enter the co-ordinate y3");
+        int y3 = sc.nextInt();
+        System.out.println("Enter the co-ordinate x4");
+        int x4 = sc.nextInt();
+        System.out.println("Enter the co-ordinate y4");
+        int y4 = sc.nextInt();
+        LineComparison result2 = new LineComparison(x3, x4, y3, y4);
+        result2.calculateLengthOfLine().show();
+
+        finalResult = compareLines(result1, result2);
+        if (finalResult == 0)
+            System.out.println("The two Lines are equal in Length");
+        else if (finalResult < 0)
+            System.out.printf("The Length of the line formed by the co-ordinates (%d,%d),(%d,%d) is greater than (%d,%d),(%d,%d)", result2.x1, result2.y1, result2.x2, result2.y2, result1.x1, result1.y1, result1.x2, result1.y2);
+        else
+            System.out.printf("The Length of the line by the co-ordinates (%d,%d) and (%d,%d) is greater than (%d,%d) and (%d,%d)", result1.x1, result1.y1, result1.x2, result1.y2, result2.x1, result2.y1, result2.x2, result2.y2);
+    }
+    public static void main(String[] args){
+        lineCoOrdinates();
+    }
+}
